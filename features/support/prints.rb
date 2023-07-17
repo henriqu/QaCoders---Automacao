ea654d5d940440d4a608fed0ee11eeab64254406
Victor Henrique <victorhenrique38@outlook.com>
2023-07-17 14:12:44 -0300

    Atualização contendo: configuração do enrironments, prints

---
+++ features/support/prints.rb
@@ -0,0 +1,10 @@
+module Prints 
+
+    def take_screenshot(name_file, folder_p)
+        file = "#{folder}/#{name_file}.png"
+        FileUtils.mkdir_p(folder) unless File.exist?
+        Capybara.page.driver.browser.save_screenshot(file) 
+        attach(file, 'image/png')
+    end
+
+end
